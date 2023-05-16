f13587378fc4e06a3f5445debc5ac8a19f577914
vipulverma23 <87273258+vipulverma23@users.noreply.github.com>
2023-05-16 20:23:30 +0530

    bugs

--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -5,55 +5,60 @@ import { Button } from "react-bootstrap";
 import { useUserAuth } from "../context/AuthContext";
 
 const SignUp = () => {
-  const [email, setEmail] = useState("");
-  const [error, setError] = useState("");
-  const [password, setPassword] = useState("");
-  const { signUp } = useUserAuth();
-  let navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [error, setError] = useState("");
+    const [password, setPassword] = useState("");
+    const { signUp } = useUserAuth();
+    let navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    try {
-      await signUp(email, password);
-      navigate("/");
-    } catch (err) {
-      setError(err.message);
-    }
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+        try {
+            await signUp(email, password);
+            navigate("/");
+        } catch (err) {
+            setError(err.message);
+        }
+    };
 
-  return (
-    <div className="outerdiv">
-      <div className="box">
-        <h1 className="heading"> Signup</h1>
-        {error && <Alert variant="danger">{error}</Alert>}
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="email" controlId="formBasicEmail">
-            <Form.Control
-              type="email"
-              placeholder="Email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
+    return (
+        <div className="outerdiv">
+            <div className="box">
+                <h1 className="heading"> Signup</h1>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <Form onSubmit={handleSubmit} className="container">
+                    <Form.Group className="email" controlId="formBasicEmail">
+                        <Form.Control
+                            type="email"
+                            placeholder="Email"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </Form.Group>
 
-          <Form.Group className="password" controlId="formBasicPassword">
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
+                    <Form.Group
+                        className="password"
+                        controlId="formBasicPassword"
+                    >
+                        <Form.Control
+                            type="password"
+                            placeholder="Password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </Form.Group>
 
-          <div className="signupbtn">
-            <Button type="Submit">Sign up</Button>
-          </div>
-        </Form>
-      </div>
-      <div className="text">
-        Already have an account? <Link to="/">Log In</Link>
-      </div>
-    </div>
-  );
+                    <div>
+                        <Button style={{ padding: 2 }} type="Submit">
+                            Sign Up
+                        </Button>
+                    </div>
+                </Form>
+            </div>
+            <div className="text">
+                Already have an account? <Link to="/">Log In</Link>
+            </div>
+        </div>
+    );
 };
 
 export default SignUp;
