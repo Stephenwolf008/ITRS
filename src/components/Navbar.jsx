e6dbc2ad58ba10b945c578d0b264ced2f4f5358b
vipulverma23 <87273258+vipulverma23@users.noreply.github.com>
2023-05-19 21:33:14 +0530

    Logout button position fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Menuitems } from "./Menuitem";
+import SignOut from "../routes/SignOut";
 
 function Navbar() {
-  return (
-    <div>
-      <nav className="Navbaritems">
-        <h1 className="navbar-logo">Namaste India</h1>
-        <ul className="nav-menu">
-          {Menuitems.map((item, index) => {
-            return (
-              <li key={index}>
-                <Link className={item.cName} to={item.url}>
-                  <i className={item.icon}></i>
-                  {item.title}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    </div>
-  );
+    return (
+        <div>
+            <nav className="Navbaritems">
+                <h1 className="navbar-logo">Namaste India</h1>
+                <ul className="nav-menu">
+                    {Menuitems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <Link className={item.cName} to={item.url}>
+                                    <i className={item.icon}></i>
+                                    {item.title}
+                                </Link>
+                            </li>
+                        );
+                    })}
+                    <SignOut />
+                </ul>
+            </nav>
+        </div>
+    );
 }
 
 export default Navbar;
--- src/routes/SignOut.jsx
+++ src/routes/SignOut.jsx
@@ -1,29 +1,46 @@
-
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button } from "@mui/material";
 import { useNavigate } from "react-router";
 import { useUserAuth } from "../context/AuthContext";
+import { Logout } from "@kiwicom/orbit-components/lib/icons";
 
-const SignIn = () => {
-  const { logOut } = useUserAuth();
-  const navigate = useNavigate();
-  const handleLogout = async () => {
-    try {
-      await logOut();
-      navigate("/");
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-  return (
-    <>
-      <div className="signout">
-        <Button onClick={handleLogout} className="signoutbtn">
-          SignOut
-        </Button>
-      </div>
-    </>
-  );
+const SignOut = () => {
+    const { logOut } = useUserAuth();
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+        try {
+            await logOut();
+            navigate("/");
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+    return (
+        <>
+            <div>
+                <Button
+                    onClick={handleLogout}
+                    className="signoutbtn"
+                    startIcon={<Logout />}
+                    sx={{
+                        color: "black",
+                        fontWeight: "bold",
+                        textTransform: "none",
+                        fontSize: "medium",
+
+                        paddingX: 7.5,
+                        paddingY: 2.5,
+                        "&:hover": {
+                            backgroundColor: "cyan",
+                            color: "white",
+                        },
+                    }}
+                >
+                    SignOut
+                </Button>
+            </div>
+        </>
+    );
 };
 
-export default SignIn;
+export default SignOut;
