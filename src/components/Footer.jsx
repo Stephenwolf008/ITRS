d0e6d4069d01968c6dd46e5d5638b81762f2162e
Rakshit kaushik <53433960+Stephenwolf008@users.noreply.github.com>
2023-05-20 01:32:54 +0530

    Opens in new page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,16 +11,16 @@ function Footer() {
       </div>
       <div className="bottom">
         <div className="bottom">
-          <a href="https://www.facebook.com/profile.php?id=100039294321169">
+          <a href="https://www.facebook.com" target="_blank">
             <i className="fa-brands fa-facebook-square"></i>
           </a>
-          <a href="https://www.instagram.com/vipul_023/">
+          <a href="https://www.instagram.com" target="_blank">
             <i className="fa-brands fa-instagram-square"></i>
           </a>
-          <a href="https://twitter.com/vipulverma2311">
+          <a href="https://twitter.com" target="_blank">
             <i className="fa-brands fa-twitter-square"></i>
           </a>
-          <a href="mailto:vipulverma2311@gmail.com">
+          <a href="mailto:vipulverma2311@gmail.com" target="_blank">
             <i className="fa-sharp fa-solid fa-envelope"></i>
           </a>
         </div>
