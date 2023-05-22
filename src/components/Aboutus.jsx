e208fdd6e53e4b2794ffa6c5f525441531a53bf4
Rakshit kaushik <53433960+Stephenwolf008@users.noreply.github.com>
2023-05-23 01:14:05 +0530

    Update Aboutus.jsx

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -5,14 +5,15 @@ function Aboutus() {
     <div className="about-us">
       <h1>About Us</h1>
       <p>
-        Namaste India is owned and managed by NamasteIndia.in . It will help the
+        Currently we are only showing plans for Uttarakhand and Delhi but
+        Namaste India will help the
         user in finding the best vacation spots according to their preferences
         and will make their vacation worthy.
       </p>
       <h1>Our Mission</h1>
       <p>
         Our mission is to touch the horizon where our capabilities may
-        successfully meet the requirements of our clients,that to with ultiate
+        successfully meet the requirements of our clients, that to with ultiate
         transparency.
       </p>
     </div>
